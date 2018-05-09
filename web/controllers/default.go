c9fa71a24edee28a57c6b333703cde0477e126b8
Lynnchen1989 <412369794@qq.com>
2018-05-09 17:53:29 +0800

    beego 模板

--- web/controllers/default.go
+++ web/controllers/default.go
@@ -11,5 +11,9 @@ type MainController struct {
 func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.html"
+	c.LayoutSections = make(map[string]string)
+	c.LayoutSections["Content"] = "applist.html"
+	//c.LayoutSections["AppTest"] = "apptest.html"
+	//c.Layout = "applist.html"
+	c.TplName = "base.html"
 }
