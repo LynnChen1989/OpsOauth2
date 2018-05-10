2ee0c29ac8b7d178c96ae83e7d0ce6eaea7021bf
Lynnchen1989 <412369794@qq.com>
2018-05-10 17:01:41 +0800

    图片上传

--- web/controllers/add.go
+++ web/controllers/add.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"github.com/astaxie/beego"
 	"fmt"
+	"log"
+	"strings"
 )
 
 type AddAppController struct {
@@ -19,8 +21,31 @@ func (aac *AddAppController) Get() {
 func (aac *AddAppController) Post() {
 	appName := aac.Input().Get("app-name")
 	appDesc := aac.Input().Get("app-description")
-	appLogo := aac.Input().Get("app-logo")
 	appCallBack := aac.Input().Get("app-callback")
-	fmt.Println(appName, appDesc, appLogo, appCallBack)
+
+	aac.SavePic(appName, "app-logo")
+	fmt.Println(appName, appDesc, appCallBack)
 	aac.Redirect("/app-added", 302)
+}
+
+// 保存图片
+func (aac *AddAppController) SavePic(appName string, domLogo string) {
+	// appName：应用名称；
+	// domLogo：logo html 的input标签name属性；
+	file, header, err := aac.GetFile(domLogo)
+	if err != nil {
+		log.Println("Handle logo error:", err)
+	}
+	initFileName := header.Filename
+	picSuffix := strings.Split(initFileName, ".")[1]
+	newFileName := fmt.Sprintf("%s.%s", appName, picSuffix)
+	logoPath := fmt.Sprintf("static/img/upload/%s", newFileName)
+	if file != nil {
+		err := aac.SaveToFile("app-logo", logoPath)
+		if err != nil {
+			log.Println("Save logo picture error:", err)
+		} else {
+			log.Println("Save logo picture success.")
+		}
+	}
 }
